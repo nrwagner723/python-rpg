219c97e3154960d6bc66439093aa634e8a6edf02
Natalie Wagner <nrwagner723@gmail.com>
2022-05-07 19:28:31 -0500

    created all dictionaries that hold the health and attacks for each enemy and hercules

---
+++ main.py
@@ -0,0 +1,33 @@
+# dictionaries holding health and each different 
+# attack and how much damage it deals
+hercules = {
+    'health': 315, 
+    'grace': 10,
+    'confuse': 15,
+    'bash': 20,
+    'ravage': 35
+}
+
+nemean_lion = {
+    'health': 200,
+    'bounce': 10,
+    'overpower': 15,
+    'blight': 20,
+    'reckoning': 35
+}
+
+lernaean_hydra = {
+    'health': 250,
+    'mislead': 10,
+    'whip': 15,
+    'bombard': 20,
+    'thrash': 35
+}
+
+cerberus = {
+    'health': 350,
+    'rumble': 10,
+    'smash': 15,
+    'maul': 20,
+    'fury': 35
+}
